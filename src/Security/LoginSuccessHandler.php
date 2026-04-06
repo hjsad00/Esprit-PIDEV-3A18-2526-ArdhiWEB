5d55ea39876846d3864f61b440409667a0b9bc22
hjsad00 <hajsalemadel10@gmail.com>
2026-04-06 20:54:46 +0100

    Corrections Design

--- src/Security/LoginSuccessHandler.php
+++ src/Security/LoginSuccessHandler.php
@@ -6,10 +6,13 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 
 class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
+    use TargetPathTrait;
+
     public function __construct(
         private RouterInterface $router,
     ) {
@@ -17,6 +20,13 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
     {
+        // Check if there is a target path saved in the session
+        $targetPath = $this->getTargetPath($request->getSession(), 'main');
+
+        if ($targetPath) {
+            return new RedirectResponse($targetPath);
+        }
+
         $user = $token->getUser();
 
         if (in_array('ROLE_ADMIN', $user->getRoles(), true)) {
