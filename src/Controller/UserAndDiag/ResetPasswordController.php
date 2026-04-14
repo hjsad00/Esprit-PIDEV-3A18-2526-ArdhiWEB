a58a0afd6a27ae4b9bd4bdef378d72b145655449
hjsad00 <hajsalemadel10@gmail.com>
2026-04-14 16:27:50 +0100

    Scan Santé

--- src/Controller/UserAndDiag/ResetPasswordController.php
+++ src/Controller/UserAndDiag/ResetPasswordController.php
@@ -76,7 +76,7 @@ class ResetPasswordController extends AbstractController
     /**
      * Validates and process the reset URL that the user clicked in their email.
      */
-    #[Route('/reset/{token}', name: 'app_reset_password')]
+    #[Route('/reset/{token?}', name: 'app_reset_password')]
     public function reset(Request $request, UserPasswordHasherInterface $passwordHasher, TranslatorInterface $translator, ?string $token = null): Response
     {
         if ($token) {
