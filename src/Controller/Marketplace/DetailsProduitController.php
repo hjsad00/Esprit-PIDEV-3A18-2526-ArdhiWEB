eae1ce041e4333e47dbb2bc7a925f30a6ce51395
saifmej <mejsaif@gmail.com>
2026-04-18 21:22:30 +0100

    amelioration de page detailsProduit

--- src/Controller/Marketplace/DetailsProduitController.php
+++ src/Controller/Marketplace/DetailsProduitController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Marketplace;
 
+use App\Repository\Marketplace\AvisRepository;
 use App\Repository\Marketplace\ProduitsRepository;
+use App\Service\Marketplace\MarketplaceQrService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +15,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class DetailsProduitController extends AbstractController
 {
     #[Route('/marketplace/produit/{id}', name: 'app_marketplace_produit_show')]
-    public function show(int $id, ProduitsRepository $produitsRepository): Response
-    {
-        $produit = $produitsRepository->find($id);
+    public function show(
+        int $id, 
+        ProduitsRepository $produitsRepo, 
+        AvisRepository $avisRepo,
+        MarketplaceQrService $marketplaceQrService
+    ): Response {
+        $produit = $produitsRepo->find($id);
 
         if (!$produit) {
             throw $this->createNotFoundException('Produit introuvable.');
         }
 
+        // Génération/Récupération du QR Code pour le produit
+        $qrPath = $marketplaceQrService->generateForProduct($produit);
+
+        // Récupération des avis avec les auteurs (Users)
+        $avisList = $avisRepo->findByProduitWithUser($produit);
+        
+        // ... rest of calculations ...
+        $nbAvis = count($avisList);
+        $avgRating = $avisRepo->getAverageNote($id);
+        
+        $starsStats = [5 => 0, 4 => 0, 3 => 0, 2 => 0, 1 => 0];
+        foreach ($avisList as $av) {
+            $n = $av->getNote();
+            if (isset($starsStats[$n])) { $starsStats[$n]++; }
+        }
+
         return $this->render('Marketplace/details.html.twig', [
             'produit' => $produit,
+            'avis' => $avisList,
+            'avgRating' => $avgRating,
+            'nbAvis' => $nbAvis,
+            'starsStats' => $starsStats,
+            'qrPath' => $qrPath
         ]);
     }
 }
