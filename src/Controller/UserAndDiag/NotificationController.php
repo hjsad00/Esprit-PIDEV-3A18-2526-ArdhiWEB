850879d2d1dd3a83234482964ea2ecf11a63008b
hjsad00 <hajsalemadel10@gmail.com>
2026-04-18 15:00:21 +0100

    Second avis

--- src/Controller/UserAndDiag/NotificationController.php
+++ src/Controller/UserAndDiag/NotificationController.php
@@ -6,6 +6,7 @@ use App\Repository\UserAndDiag\DiagNotificationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -43,5 +44,29 @@ class NotificationController extends AbstractController
         $notifRepo->markAllAsReadForUser($user);
 
         return $this->json(['success' => true]);
+    }
+
+    #[Route('/read-and-redirect/{id}', name: 'app_user_and_diag_notifications_read_redirect', methods: ['GET'])]
+    public function readAndRedirect(\App\Entity\UserAndDiag\DiagNotification $notif, EntityManagerInterface $em): Response
+    {
+        if ($notif->getUser() === $this->getUser()) {
+            $notif->setRead(true);
+            $em->flush();
+        }
+
+        $link = '/';
+        $rt = $notif->getRelatedEntityType();
+        $rid = $notif->getRelatedEntityId();
+
+        if ($rt === 'POST')
+            $link = '/user-and-diag/community/' . $rid;
+        elseif ($rt === 'TREATMENT')
+            $link = '/user-and-diag/treatment-plan/' . $rid . '/details';
+        elseif ($rt === 'PREVENTION')
+            $link = '/user-and-diag/prevention-plan/' . $rid . '/details';
+        elseif ($rt === 'DIAGNOSIS')
+            $link = '/user-and-diag/history';
+
+        return $this->redirect($link);
     }
 }
